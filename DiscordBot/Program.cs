2dcf83cbacc2d723afd5ea0cb97c95c996134807
Siddharth Saxena <sidd.saxena96@gmail.com>
2023-04-26 20:13:31 +0530

    Routine job time changed to 30 min

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -17,7 +17,7 @@ namespace DiscordBot
             CancellationTokenSource cts = new CancellationTokenSource();
 
             // Create a periodic timer that will call the DoWork method every 5 minutes
-            TimeSpan period = TimeSpan.FromSeconds(30);
+            TimeSpan period = TimeSpan.FromMinutes(30);
             using var timer = new Timer(async _ => await DoWork(), null, TimeSpan.Zero, period);
 
             // Wait for the user to press a key to cancel the timer
